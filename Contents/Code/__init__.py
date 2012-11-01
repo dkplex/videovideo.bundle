11b0b6c422f0ffa202af6f11ea9a6f5d239ae136
sander1 <sander@plexapp.com>
2012-11-01 01:33:40 +0100

    Uhm, changes 'n stuff

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,91 +1,74 @@
-TITLE       = 'VideoVideo.dk'
-PATH        = 'http://www.videovideo.dk/'
-ART         = 'art-default.jpg'
-ICON        = 'icon-default.png'
+TITLE = 'videovideo.dk'
+SHOWS_URL = 'http://www.videovideo.dk/shows/json'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ###################################################################################################
 def Start():
 
-    # Plugin setup
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	# Plugin setup
+	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-    # Object / Directory / VideoClip setup
-    ObjectContainer.title1      = TITLE
-    ObjectContainer.view_group  = 'InfoList'
-    ObjectContainer.art         = R(ART)
-    DirectoryObject.thumb       = R(ICON)
-    DirectoryObject.art         = R(ART)
-    VideoClipObject.thumb       = R(ICON)
-    VideoClipObject.art         = R(ART)
+	# Object / Directory / VideoClip setup
+	ObjectContainer.title1 = TITLE
+	ObjectContainer.view_group = 'InfoList'
+	ObjectContainer.art = R(ART)
+	DirectoryObject.thumb = R(ICON)
+	DirectoryObject.art = R(ART)
+	VideoClipObject.thumb = R(ICON)
+	VideoClipObject.art = R(ART)
 
-    # HTTP setup
-    HTTP.CacheTime              = CACHE_1HOUR
-    HTTP.Headers['User-Agent']  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0'
+	# HTTP setup
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0'
 
 ###################################################################################################
 @handler('/video/videovideo', TITLE, thumb=ICON, art=ART)
 def MainMenu():
 
-    # create container
-    oc = ObjectContainer()
-    shows = JSON.ObjectFromURL(PATH + 'shows/json')
+	oc = ObjectContainer()
+	shows = JSON.ObjectFromURL(SHOWS_URL)
 
-    for show in shows:
-        # add elements to container
-        oc.add(DirectoryObject(
-                               key      = Callback(BrowseShow, 
-                                                     title = show.get('title'),
-                                                     url = show.get('url')),
-                               title    = show.get('title'),
-                               thumb    = Resource.ContentsOfURLWithFallback(url=show.get('image'), fallback=ICON),
-                               summary  = show.get('description')
-                               ))
+	for show in shows:
+		oc.add(DirectoryObject(
+			key = Callback(BrowseShow,
+				title = show.get('title'),
+				url = show.get('url')),
+			title = show.get('title'),
+			thumb = Resource.ContentsOfURLWithFallback(url=show.get('image'), fallback=ICON),
+			summary = show.get('description')
+		))
 
-    return oc
+	return oc
 
 ###################################################################################################
 @route('/video/videovideo/show')
-def BrowseShow(title = TITLE, url = ''):
+def BrowseShow(title, url):
 
-    # create container
-    oc = ObjectContainer(view_group="List", title1 = TITLE, title2 = title)
+	oc = ObjectContainer(view_group="List", title2=title)
+	episodes = JSON.ObjectFromURL(url)
 
-    try:
-        episodes = JSON.ObjectFromURL(url)
+	for episode in episodes:
+		oc.add(VideoClipObject(
+			url = video.get('url'),
+			title = video.get('title'),
+			summary = video.get('shownotes'),
+			thumb = Resource.ContentsOfURLWithFallback(url=video.get('image'), fallback=ICON),
+			duration = TimeToMs(video.get('duration')),
+			originally_available_at = Datetime.ParseDate(video.get('timestamp')).date()
+		))
 
-    except:
-        raise Ex.MediaNotAvailable
-
-    for episode in episodes:
-        # add elements to container
-        oc.add(getVideoObj(episode))
-
-    return oc
+	return oc
 
 ###################################################################################################
+def TimeToMs(timecode):
 
-def getVideoObj(video = dict):
-    
-    # find video url
-    if video.get('distributions').get('720'):
-        url = video.get('distributions').get('720')
-    elif video.get('distributions').get('480'): 
-        url = video.get('distributions').get('720')
-    
-    # find duration
-    duration = 0
-    if video.get('duration'): 
-        duration_dict = video.get('duration').split(':')
-        duration += int(duration_dict[0])*60*60
-        duration += int(duration_dict[1])*60
-        duration += int(duration_dict[2])
-        duration = duration * 1000
-    
-    return  VideoClipObject(url = url, 
-                            title = video.get('title'), 
-                            summary = video.get('shownotes'), 
-                            thumb = Resource.ContentsOfURLWithFallback(url=show.get('image'), fallback=ICON),
-                            duration = duration,
-                            originally_available_at = Datetime.ParseDate(video.get('timestamp')))
-    
+	seconds  = 0
+	duration = timecode.split(':')
+	duration.reverse()
+
+	for i in range(0, len(duration)):
+		seconds += int(duration[i]) * (60**i)
+
+	return seconds * 1000
